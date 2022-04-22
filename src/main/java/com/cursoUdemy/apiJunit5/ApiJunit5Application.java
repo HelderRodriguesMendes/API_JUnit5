a9833647eb5c66c8fda61748e16831e6afe7a6d1
helde <helderrodrigues37@gmail.com>
2022-04-22 18:23:47 -0300

    Criando projeto

---
+++ src/main/java/com/cursoUdemy/apiJunit5/ApiJunit5Application.java
@@ -0,0 +1,13 @@
+package com.cursoUdemy.apiJunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoUdemy/apiJunit5/ApiJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoUdemy.apiJunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
