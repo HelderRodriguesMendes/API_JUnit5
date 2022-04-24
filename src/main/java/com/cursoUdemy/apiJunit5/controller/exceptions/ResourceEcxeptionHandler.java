21c12e958024f2358f0cd44857cf72cd96683f2d
helde <helderrodrigues37@gmail.com>
2022-04-23 23:30:22 -0300

    Validando se email ja existe

--- src/main/java/com/cursoUdemy/apiJunit5/controller/exceptions/ResourceEcxeptionHandler.java
+++ src/main/java/com/cursoUdemy/apiJunit5/controller/exceptions/ResourceEcxeptionHandler.java
@@ -1,5 +1,6 @@
 package com.cursoUdemy.apiJunit5.controller.exceptions;
 
+import com.cursoUdemy.apiJunit5.service.exceptions.DataIntegratyViolationException;
 import com.cursoUdemy.apiJunit5.service.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,11 @@ public class ResourceEcxeptionHandler {
     public ResponseEntity<StandarError>objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
         StandarError error = new StandarError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandarError>dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request){
+        StandarError error = new StandarError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/cursoUdemy/apiJunit5/service/UserService.java
+++ src/main/java/com/cursoUdemy/apiJunit5/service/UserService.java
@@ -2,11 +2,13 @@ package com.cursoUdemy.apiJunit5.service;
 
 import com.cursoUdemy.apiJunit5.model.User;
 import com.cursoUdemy.apiJunit5.repository.UserRepository;
+import com.cursoUdemy.apiJunit5.service.exceptions.DataIntegratyViolationException;
 import com.cursoUdemy.apiJunit5.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public User save(User user){
+        validEmail(user.getEmail());
         return userRepository.save(user);
     }
 
@@ -28,5 +31,12 @@ public class UserService {
 
     public List<User> findByName(String name){
         return userRepository.findBynameLike("%"+name+"%");
+    }
+
+    private void validEmail(String email){
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if(userOptional.isPresent()){
+            throw new DataIntegratyViolationException("O email informado já esta cadastrado");
+        }
     }
 }
