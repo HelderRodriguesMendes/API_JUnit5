d652e7eea3485e05302df9c9680f6a505fb7b081
helde <helderrodrigues37@gmail.com>
2022-05-04 21:35:17 -0300

    Teste unitario para update de Users

--- src/test/java/com/cursoUdemy/apiJunit5/service/UserServiceTest.java
+++ src/test/java/com/cursoUdemy/apiJunit5/service/UserServiceTest.java
@@ -126,7 +126,15 @@ class UserServiceTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSUCCESS() {
+        when(userRepository.save(any())).thenReturn(user);
+        User response = userService.update(user);
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
